56e5f4aac5af8d3b931849b403fea6829ab08881
Garik Khachatryan <gvkhacha@uci.edu>
2018-11-26 11:46:57 -0800

    updating pipfile and bot.py to using discord-rewrite 1.0.0a

--- bot.py
+++ bot.py
@@ -1,32 +1,23 @@
 # Start date November 17th, 2018
 # Python v3.6
 
-import discord
 from discord.ext import commands
 
-class MyClient(discord.Client):
-    async def on_ready(self):
-        print('Logged in as: ')
-        print(self.user.name)
-        print(self.user.id)
-        print('------')
 
+bot = commands.Bot(command_prefix='!')
 
-    async def on_message(self, message):
-        # we do not want the bot to reply to itself
-        if message.author.id == self.user.id:
-            return
+@bot.event
+async def on_ready():
+    print('Logged in as: ')
+    print(bot.user.name)
+    print(bot.user.id)
+    print('------')
+    
+@bot.command()
+async def hello(ctx: "Context"):
+    """
+    If a user types !hello the bot will respond with 'Hello! (nameOfUser here)
+    """
+    await ctx.send(f"Hello! {ctx.message.author.mention}")
 
-        if message.content.startswith("!hello"):
-            await client.send_message(message.channel, content=(f"Hello! {message.author.mention}"))
-
-
-if __name__ == '__main__':
-    try:
-        with open("token.txt", 'r') as file:
-            TOKEN = file.read()
-            client = MyClient()
-            client.run(TOKEN)
-
-    except FileNotFoundError:
-        pass
+bot.run(TOKEN)
